92cb9aaa12d143e5fbc264be96c3fa432eee7751
shimizu hiroaki <shimizu.r.hiroaki@gmail.com>
2018-09-13 23:09:28 +0900

    commit

---
+++ init.tool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### install alp ###
+aptitude install unzip htop
+cd /usr/local/src/
+wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+mv alp /usr/local/bin/
