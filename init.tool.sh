934f13bdb3f1caa2696f64acf6d8a030b2bd139c
root <shimizu.r.hiroaki@gmail.com>
2019-09-07 13:57:26 +0900

    commit

--- init.tool.sh
+++ init.tool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### install alp ###
-aptitude install unzip htop expect
+apt install unzip htop expect
 cd /usr/local/src/
 wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
