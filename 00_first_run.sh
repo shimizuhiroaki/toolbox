58dcbf7934a05894e9a63348d42c351ab48d9a9c
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 12:38:18 +0900

    alp

--- 00_first_run.sh
+++ 00_first_run.sh
@@ -3,6 +3,7 @@
 ### install alp ###
 apt install unzip htop expect git iftop iotop dstat lv net-tools
 cd /usr/local/src/
-wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
+#wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
+wget -d https://github.com/tkuchiki/alp/releases/download/v1.0.3/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
 mv alp /usr/local/bin/
