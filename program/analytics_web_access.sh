228c717be5893c148390c625d4412a41ff2671dc
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 18:44:41 +0900

    alp

--- program/analytics_web_access.sh
+++ program/analytics_web_access.sh
@@ -7,27 +7,23 @@ OUTPUT=$SCRIPT_DIR/data/web.txt
 
 echo -e "[ WEB LOG -- $DATE ]\\n" >> $OUTPUT.$DATE
 
-echo -e "[ TOP10 COUNT ]\\n" >> $OUTPUT.$DATE
-#alp -f $ALP_LOG --cnt -r  >> $OUTPUT.$DATE
+echo -e "[ (1) TOP10 COUNT ]\\n" >> $OUTPUT.$DATE
 cat $ALP_LOG | alp ltsv --sort=count --reverse | head -n 13 >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
-echo -e "[ TOP10 SUM ]\\n" >> $OUTPUT.$DATE
+echo -e "[ (2) TOP10 SUM ]\\n" >> $OUTPUT.$DATE
 cat $ALP_LOG | alp ltsv --sort=sum --reverse | head -n 13 >> $OUTPUT.$DATE
-# alp -f $ALP_LOG --sum -r | head -n 13 >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
-echo -e "[ TOP10 AVG ]\\n" >> $OUTPUT.$DATE
-#alp -f $ALP_LOG --avg -r | head -n 13 >> $OUTPUT.$DATE
+echo -e "[ (3) TOP10 AVG ]\\n" >> $OUTPUT.$DATE
 cat $ALP_LOG | alp ltsv --sort=avg --reverse | head -n 13 >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
-echo -e "[ ALL LOGS ]\\n" >> $OUTPUT.$DATE
-#alp -f $ALP_LOG --cnt -r  >> $OUTPUT.$DATE
+echo -e "[ (4) ALL LOGS ]\\n" >> $OUTPUT.$DATE
 cat $ALP_LOG | alp ltsv --sort=count --reverse --query-string >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
 cat $OUTPUT.$DATE > $OUTPUT
-#echo "" > $ALP_LOG
+echo "" > $ALP_LOG
 
 exit 0
