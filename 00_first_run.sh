c070a8bbce83883efdb463d680619ee9ce3baac9
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 12:15:42 +0900

    first

--- 00_first_run.sh
+++ 00_first_run.sh
@@ -5,5 +5,4 @@ apt install unzip htop expect git iftop iotop dstat lv net-tools
 cd /usr/local/src/
 wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
-git clone https://github.com/shimizuhiroaki/toolbox.git
 mv alp /usr/local/bin/
