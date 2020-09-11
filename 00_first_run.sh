bd5a92dc9186986021e25c01114703cd7a1b61e0
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 16:02:38 +0900

    nginx

--- 00_first_run.sh
+++ 00_first_run.sh
@@ -17,3 +17,11 @@ cd /usr/local/src/
 wget -d https://github.com/tkuchiki/alp/releases/download/v1.0.3/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
 mv alp /usr/local/bin/
+
+### disable unneed processes ###
+#systemctl disable iscsid
+#systemctl disable lvm2-lvmetad
+#systemctl disable mdadm
+#systemctl disable snapd
+
+exit 0
