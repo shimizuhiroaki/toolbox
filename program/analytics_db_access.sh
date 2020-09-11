a83720dd02e56b4c254004609e2015e15fa53c7d
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 19:10:47 +0900

    dbanalytics

--- program/analytics_db_access.sh
+++ program/analytics_db_access.sh
@@ -7,15 +7,15 @@ OUTPUT=$SCRIPT_DIR/data/db.txt
 
 echo -e "[ DB LOG -- $DATE ]\\n" >> $OUTPUT.$DATE
 
-echo -e "[ Query Time ]\\n" >> $OUTPUT.$DATE
-/usr/bin/mysqldumpslow -s t $MYSQL_SLOW_LOG >> $OUTPUT.$DATE
+echo -e "[(1) Top 5 Query Time ]\\n" >> $OUTPUT.$DATE
+/usr/bin/mysqldumpslow -s t $MYSQL_SLOW_LOG | head -n 15 >> $OUTPUT.$DATE
 
-echo -e "[ TOP Query Count ]\\n" >> $OUTPUT.$DATE
-/usr/bin/mysqldumpslow -s c $MYSQL_SLOW_LOG | head -n 20 >> $OUTPUT.$DATE
+echo -e "[(2) TOP 5 Query AVG ]\\n" >> $OUTPUT.$DATE
+/usr/bin/mysqldumpslow -s at $MYSQL_SLOW_LOG | head -n 15 >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
-echo -e "[ TOP Query AVG ]\\n" >> $OUTPUT.$DATE
-/usr/bin/mysqldumpslow -s at $MYSQL_SLOW_LOG | head -n 20 >> $OUTPUT.$DATE
+echo -e "[(3) TOP 5 Query Count ]\\n" >> $OUTPUT.$DATE
+/usr/bin/mysqldumpslow -s c $MYSQL_SLOW_LOG | head -n 15 >> $OUTPUT.$DATE
 echo -e "\\n\\n" >> $OUTPUT.$DATE
 
 cat $OUTPUT.$DATE > $OUTPUT
