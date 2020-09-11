4cc19502904d921ef17f5e9629279235dd02c307
shimizu <shimizu.r.hiroaki@gmail.com>
2020-09-11 13:24:05 +0900

    vim

--- 00_first_run.sh
+++ 00_first_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+### install needs ###
+apt install unzip htop expect git iftop iotop dstat lv net-tools arping nkf unzip dnsutils
+
 ### install alp ###
-apt install unzip htop expect git iftop iotop dstat lv net-tools
 cd /usr/local/src/
 wget -d https://github.com/tkuchiki/alp/releases/download/v0.3.1/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
